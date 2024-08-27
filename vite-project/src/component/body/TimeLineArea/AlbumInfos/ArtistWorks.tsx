ef205c031807979748937f59926395aa0e0f1340
Taichi-iskw <ishikawataichi1@gmail.com>
2024-08-27 21:46:56 +0900

    show album images

--- vite-project/src/component/body/TimeLineArea/AlbumInfos/ArtistWorks.tsx
+++ vite-project/src/component/body/TimeLineArea/AlbumInfos/ArtistWorks.tsx
@@ -1,69 +1,63 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
+import ImageComponent from '../../SearchArea/suggestedArtist/ImageComponent';
+
 interface ArtistWorksProps {
     id: string;
 }
+
 interface AlbumInfo {
     id: string;
     name: string;
+    release_date: string;
+    images: { url: string }[];
 }
 
 const ArtistWorks: React.FC<ArtistWorksProps> = ({ id }) => {
     const [albums, setAlbums] = useState<AlbumInfo[]>([]);
 
     useEffect(() => {
-        const getWorks = async (id: string) => {
+        const getAllAlbums = async (id: string) => {
             try {
                 const token = import.meta.env.VITE_SPOTIFY_TOKEN;
                 const BASE_URL = import.meta.env.VITE_SPOTIFY_BASE_URL;
-                const END_POINT = `${BASE_URL}artists/${id}/albums`;
+                let allAlbums: AlbumInfo[] = [];
+                let offset = 0;
+                let totalAlbums = 0;
 
-                const res = await axios.get(END_POINT, {
-                    params: {
-                        include_groups: 'album',
-                        limit: '50',
-                        offset: '0',
-                    },
-                    headers: {
-                        Authorization: `Bearer ${token}`,
-                    },
-                });
-                console.log(res.data.items);
-                ////////////////////
-                const albums = res.data.items;
-                const revAlbums = albums.reverse();
-                const uniqueAlbumNames: Set<string> = new Set();
+                do {
+                    const END_POINT = `${BASE_URL}artists/${id}/albums`;
 
-                const uniqueAlbums = revAlbums.filter((album) => {
-                    const albumName = album.name;
-                    let mainName = albumName;
+                    const res = await axios.get(END_POINT, {
+                        params: {
+                            include_groups: 'album',
+                            limit: '50',
+                            offset: offset.toString(),
+                        },
+                        headers: {
+                            Authorization: `Bearer ${token}`,
+                        },
+                    });
 
-                    if (albumName.includes('(')) {
-                        mainName = albumName.split('(')[0].trim();
-                    } else if (albumName.includes('-')) {
-                        mainName = albumName.split('-')[0].trim();
-                    }
+                    const albums = res.data.items;
+                    totalAlbums = res.data.total;
+                    allAlbums = allAlbums.concat(albums);
+                    offset += 50; // 次のページへ
+                } while (offset < totalAlbums);
 
-                    const isUnique = !uniqueAlbumNames.has(mainName);
-                    if (isUnique) {
-                        console.log(mainName, album.release_date);
-                        uniqueAlbumNames.add(mainName);
-                    }
+                // 時系列順にソート
+                const sortedAlbums = allAlbums.sort((a, b) =>
+                    new Date(a.release_date) > new Date(b.release_date) ? 1 : -1,
+                );
 
-                    return isUnique;
-                });
-
-                console.log(uniqueAlbums);
-
-                ////////////////
-                setAlbums(uniqueAlbums);
+                setAlbums(sortedAlbums);
             } catch (error) {
                 console.error('Error fetching artist albums:', error);
             }
         };
 
-        getWorks(id);
+        getAllAlbums(id);
     }, [id]);
 
     return (
@@ -71,7 +65,10 @@ const ArtistWorks: React.FC<ArtistWorksProps> = ({ id }) => {
             <h2>Artist ID: {id}</h2>
             <ul>
                 {albums.map((album) => (
-                    <li key={album.id}>{album.name}</li>
+                    <li key={album.id}>
+                        <ImageComponent image={album.images[0]} />
+                        {album.release_date + ' : ' + album.name}
+                    </li>
                 ))}
             </ul>
         </div>
